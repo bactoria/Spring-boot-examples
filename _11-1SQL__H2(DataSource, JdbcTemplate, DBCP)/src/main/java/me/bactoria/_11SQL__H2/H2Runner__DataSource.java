a99ca081e518746537af9cf58d2c106065afa124
bactoria <bactoria@gmail.com>
2018-10-18 17:09:17 +0900

    Add dbcp

--- _11-1SQL__H2(DataSource, JdbcTemplate, DBCP)/src/main/java/me/bactoria/_11SQL__H2/H2Runner__DataSource.java
+++ _11-1SQL__H2(DataSource, JdbcTemplate, DBCP)/src/main/java/me/bactoria/_11SQL__H2/H2Runner__DataSource.java
@@ -22,9 +22,32 @@ public class H2Runner__DataSource implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+        /*
+        dependency에 jdbc를 추가하면 Hikari 가 들어옴.
+        DBCP 중 하나임.
+        스프링부트는 Hikari DBCP를 Default로 사용함.
+
+        -> DataSourceAutoConfiguration.java 에서
+        PooledDataSourceConfiguration를 보면
+        Hikari - Tomcat - Dbcp2 순서로 import 하는 것을 볼 수 있음.
+
+        =>  classpath에 HikariCP, Tomcat CP, Commons DBCP2
+         3개가 다 있으면 HikariCP를 사용함.
+
+        Hikari CP 상세 (Default 확인 가능) - connectionTimeout: 30second / maximumPoolSize: 10
+        https://github.com/brettwooldridge/HikariCP#frequently-used
+        */
+
+        /*
+        동시에 일을 할 수 있는 connection은 CPU의 코어 갯수와 같다.
+        connectionPool 개수를 잘 생각해야 함.
+        */
+
         try (Connection connection = dataSource.getConnection()) {
 
             // connection 정보 찍기
+            logger.info(dataSource.toString());
             logger.info(connection.getMetaData().getURL());
             logger.info(connection.getMetaData().getUserName());
 
